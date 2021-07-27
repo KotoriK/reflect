57b0f906666bd27ced7c8c76e98140385b7c8409
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-07-27 14:16:57 +0800

    update headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 const withMDX = require('@next/mdx')({
     extension: /\.mdx?$/
   })
+const _headers = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  }
+]
   module.exports = withMDX({
-    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx']
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+    async headers() {
+      return [
+        {
+          // Apply these headers to all routes in your application.
+          source: '/(.*)',
+          headers:_headers,
+        },
+      ]
+    },
   })
